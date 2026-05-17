c23397a7ea0b184ddbe7a9aec9a6bf3eab41bedf
Vineet Ramshetty <vineet.ramshetty@gmail.com>
2026-05-17 18:22:33 +0530

    files uploaded for deployment

---
+++ app.js
@@ -0,0 +1,98 @@
+if(process.env.NODE_ENV!="production"){
+    require('dotenv').config();
+}
+
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+const User=require("./models/user.js");
+const ExpressError=require("./utils/ExpressError.js");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const session=require("express-session");
+const {MongoStore} = require('connect-mongo');
+const flash=require("connect-flash");
+const mongoose=require("mongoose");
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+const express=require("express");
+const app=express();
+
+let port=3000;
+
+
+const DB_URL=process.env.ATLASDB_URL;
+
+main().then((res)=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(DB_URL);
+};
+
+const store=MongoStore.create({
+    mongoUrl: DB_URL,
+    crypto:{
+        secret: process.env.SECRET
+    },
+    touchAfter: 24*3600
+});
+
+store.on("error", (err)=>{
+    console.log("ERROR IN MONGO SESSION STORE", err);
+});
+
+const sessionOptions={
+    store:store,
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now()+7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly:true
+    }
+};
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine("ejs", ejsMate);
+app.use(session(sessionOptions));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next)=>{
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    res.locals.currUser=req.user;
+    return next();
+});
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+app.use((req, res, next)=>{
+    return next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next)=>{
+    let {statusCode=500, message="Something went wrong!"}=err;
+    res.status(statusCode).render("error", {message});
+});
+
+app.listen(port, ()=>{
+    console.log(`app is listening on port: ${port}`);
+});
